d00ee9062022bc3a6037a279295e86deeb864f1a
Capstone_GreenGuard <larasayodya04@gmail.com>
2025-04-28 20:08:38 +0700

    balikin lagi haha

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,11 @@ import NotesRoutes from "./routes/NotesRoutes.js";
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(NotesRoutes);
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(5000, '0.0.0.0', () => {
     console.log("Server berjalan di port 5000");
 });
